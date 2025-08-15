3e9697caa4da731a4b2c9c698f66156bf14cf2c3
alfanandajeremy <115684344+alfanandajeremy@users.noreply.github.com>
2025-08-15 16:59:54 +0700

    Update appgroqlog.py

--- appgroqlog.py
+++ appgroqlog.py
@@ -128,7 +128,7 @@ def main_app():
         st.subheader("Model")
         model_option = st.selectbox(
             'Pilih model yang akan digunakan:',
-            ('whisper-large-v3-turbo','qwen/qwen3-32b','deepseek-r1-distill-llama-70b','llama-3.3-70b-versatile','deepseek-r1-distill-llama-70b')
+            ('meta-llama/llama-guard-4-12b','qwen/qwen3-32b','deepseek-r1-distill-llama-70b','llama-3.3-70b-versatile','deepseek-r1-distill-llama-70b')
         )
 
         if uploaded_file:
@@ -245,6 +245,7 @@ if st.session_state.authenticated:
     main_app()
 else:
     login_page()
+
 
 
 
