49f22277c3c3590e78154d62c5dac37b50707611
alfanandajeremy <115684344+alfanandajeremy@users.noreply.github.com>
2025-09-16 16:08:18 +0700

    Update appgroqlog.py

--- appgroqlog.py
+++ appgroqlog.py
@@ -452,7 +452,7 @@ def main_app():
         st.subheader(f"Selamat datang, {st.session_state.get('username','')}!")
 
         if 'GROQ_API_KEY' not in st.session_state:
-            st.session_state.GROQ_API_KEY = st.secrets.get("GROQ_API_KEY", "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").strip()
+            st.session_state.GROQ_API_KEY = st.secrets.get("GROQ_API_KEY", "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").strip()
 
         st.subheader("Unggah File")
         st.markdown("Unggah file untuk dianalisis.")
@@ -614,4 +614,5 @@ if st.session_state.authenticated:
     main_app()
 else:
     login_page()
+
 
